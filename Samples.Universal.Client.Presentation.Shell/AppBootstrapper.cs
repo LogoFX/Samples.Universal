f40b21fe7445338453ceb51e9749f4cc812b175e
godrose <godrose@gmail.com>
2016-03-31 15:22:06 +0300

    Updated Bootstrapping packages.

--- Samples.Universal.Client.Presentation.Shell/AppBootstrapper.cs
+++ Samples.Universal.Client.Presentation.Shell/AppBootstrapper.cs
@@ -1,4 +1,3 @@
-using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml.Controls;
 using Caliburn.Micro;
 using LogoFX.Client.Bootstrapping;
@@ -14,17 +13,6 @@ namespace Samples.Universal.Client.Presentation.Shell
         public AppBootstrapper() : base(_iocContainer, c => new WinRTContainerAdapter(c))
         {
         }
-
-        protected override void BeforeOnLaunched(LaunchActivatedEventArgs e)
-        {
-            System.Windows.Threading.Dispatch.Current.InitializeDispatch();
-#if DEBUG
-            //if (Debugger.IsAttached)
-            //{
-            //    DebugSettings.EnableFrameRateCounter = true;
-            //}
-#endif  
-        }        
 
         protected override void PrepareViewFirst(Frame rootFrame)
         {
