39c3f1974babb523e17b4c757218d3a424078d8b
Gennady Verdel <godrose@gmail.com>
2019-01-14 11:38:50 +0200

    Minor

--- Samples.Universal.Launcher/AppBootstrapper.cs
+++ Samples.Universal.Launcher/AppBootstrapper.cs
@@ -17,7 +17,7 @@ namespace Samples.Universal.Launcher
 
         public override CompositionOptions CompositionOptions => new CompositionOptions
         {
-            Prefixes = new [] { "Samples.Universal" }
+            Prefixes = new [] { "Samples.Universal", "Samples.Client" }
         };
 
         protected override void PrepareViewFirst(Frame rootFrame)
