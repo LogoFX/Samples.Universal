794f68cc2b02f268be0f74a6cb8384d55034b12e
dmitry.bublik <dmitry.buba@gmail.com>
2019-01-16 14:51:01 +0200

    fix: remove LightMoq

--- Samples.Client.Data.Fake.ProviderBuilders/LoginProviderBuilder.cs
+++ Samples.Client.Data.Fake.ProviderBuilders/LoginProviderBuilder.cs
@@ -1,34 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using Attest.Fake.Builders;
-using Attest.Fake.LightMock;
-using Attest.Fake.Setup;
-using LightMock;
+using Attest.Fake.Moq;
+using Attest.Fake.Setup.Contracts;
 using Samples.Client.Data.Contracts.Providers;
 
 namespace Samples.Client.Data.Fake.ProviderBuilders
 {               
-    public class LoginProviderBuilder : FakeBuilderBase<ILoginProvider>
+    public class LoginProviderBuilder : FakeBuilderBase<ILoginProvider>.WithInitialSetup
     {
-        class LoginProviderProxy : ProviderProxyBase<ILoginProvider>, ILoginProvider
-        {
-            public LoginProviderProxy(IInvocationContext<ILoginProvider> context)
-                : base(context)
-            {
-            }
-
-            public Task Login(string username, string password)
-            {
-                return Invoke(t => t.Login(username, password));
-            }
-        }
-
         private readonly List<Tuple<string, string>> _users = new List<Tuple<string, string>>();
         private readonly Dictionary<string, bool> _isLoginAttemptSuccessfulCollection = new Dictionary<string, bool>();
         
         private LoginProviderBuilder()
-            :base(FakeFactoryHelper.CreateFake<ILoginProvider>(c => new LoginProviderProxy(c)))
         {
 
         }
@@ -43,25 +27,19 @@ namespace Samples.Client.Data.Fake.ProviderBuilders
             _users.Add(new Tuple<string, string>(username, password));            
         }
 
-        protected override void SetupFake()
-        {            
-            var initialSetup = ServiceCallFactory.CreateServiceCall(FakeService);
-
-            var setup = initialSetup
-               .AddMethodCallAsync<string, string>(t => t.Login(The<string>.IsAnyValue, The<string>.IsAnyValue),
-                    (r, login, password) =>
-                           _isLoginAttemptSuccessfulCollection.ContainsKey(login)
-                               ? _isLoginAttemptSuccessfulCollection[login]
-                                   ? r.Complete()
-                                   : r.Throw(new Exception("unable to login"))
-                               : r.Throw(new Exception("unable to login")));           
-
-            setup.Build();
-        }
-
         public void WithSuccessfulLogin(string username)
         {
             _isLoginAttemptSuccessfulCollection[username] = true;
         }
+
+        protected override IServiceCall<ILoginProvider> CreateServiceCall(
+            IHaveNoMethods<ILoginProvider> serviceCallTemplate) => serviceCallTemplate
+            .AddMethodCall<string, string>(t => t.Login(It.IsAny<string>(), It.IsAny<string>()),
+                (r, login, password) =>
+                    _isLoginAttemptSuccessfulCollection.ContainsKey(login)
+                        ? _isLoginAttemptSuccessfulCollection[login]
+                            ? r.Complete()
+                            : r.Throw(new Exception("unable to login"))
+                        : r.Throw(new Exception("unable to login")));
     }
 }
--- Samples.Client.Data.Fake.Providers/FakeLoginProvider.cs
+++ Samples.Client.Data.Fake.Providers/FakeLoginProvider.cs
@@ -10,24 +10,18 @@ namespace Samples.Client.Data.Fake.Providers
     [UsedImplicitly]
     class FakeLoginProvider : FakeProviderBase<LoginProviderBuilder, ILoginProvider>, ILoginProvider
     {
-        private readonly LoginProviderBuilder _loginProviderBuilder;
-
         public FakeLoginProvider(
             LoginProviderBuilder loginProviderBuilder,
             IUserContainer userContainer)
+            : base(loginProviderBuilder)
         {
-            _loginProviderBuilder = loginProviderBuilder;
             foreach (var user in userContainer.Users)
             {
-                _loginProviderBuilder.WithUser(user.Item1, user.Item2);
-                _loginProviderBuilder.WithSuccessfulLogin(user.Item1);
+                loginProviderBuilder.WithUser(user.Item1, user.Item2);
+                loginProviderBuilder.WithSuccessfulLogin(user.Item1);
             }
         }
 
-        async Task ILoginProvider.Login(string username, string password)
-        {
-            var service = GetService(() => _loginProviderBuilder, b => b);
-            await service.Login(username, password);
-        }
-    }    
+        void ILoginProvider.Login(string username, string password) => GetService().Login(username, password);
+    }
 }
